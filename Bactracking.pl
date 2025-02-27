7aa0db48eabf9ed397ad85766650070fcb08c361
WILLIAM-863 <william.sanchez@utp.edu.co>
2025-02-27 01:32:28 -0500

    conexiones

---
+++ Bactracking.pl
@@ -0,0 +1,9 @@
+conexion(vancouver,edmonton,16).
+conexion(vancouver,calgary,13).
+conexion(edmonton,saskatoon,12).
+conexion(saskatoon,calgary,9).
+conexion(saskatoon,winnipeg,20).
+conexion(calgary,edmonton,4).
+conexion(calgary,regina,14).
+conexion(regina,saskatoon,7).
+conexion(regina,winnipeg,4).
